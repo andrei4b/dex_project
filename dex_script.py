23309d047672d16e8ff10204d0af9f7ddbb5ec3a
Andrei B <andrei4.bulzan@gmail.com>
2018-07-07 08:54:48 +0300

    latest

--- dex_script.py
+++ dex_script.py
@@ -1,3 +1,39 @@
+def valid_sol(w1,w2):
+    prefixes = ['re', 'de', 'co', 'pre', 'in', 'pro', 'di', 'ca', 'cu', 
+    'ba', 'des', 'al', 'bi', 'mi', 'ta', 'tri', 'ex', 'ar', 'st', 'anti', 
+    'para', 'im', 'per', 'ci', 'mono', 'ce', 'că', 'la', 'si', 'dis', 'mă', 
+    'poli', 'or', 'mu', 'fi', 'ga', 'cur', 'ha', 'sur', 'col', 
+    'ad', 'no', 'tele', 'bă', 'fa', 'ter', 'aero', 'să', 'pu', 'na', 'va', 
+    'inter', 'câr', 'ti', 'dec', 'for', 'sta', 'bur', 'șu', 'uni', 
+    'peri', 'pal', 'ap', 'bor', 'pan', 'ana', 'deca', 'supra', 'sal', 'hi', 
+    'șo', 'și', 'sil', 'tal', 'mus', 'neo', 'op', 'ante', 'cop', 'post', 'ser', 
+    'mol', 'sin', 'bal', 'za', 'ni', 'ho', 'pic', 'mega', 'hă', 'mas', 'înde', 'prea', 
+    'hidro', 'cont', 'tar', 'por', 'pis', 'sul', 'super', 'băl', 'he', 'cam', 'meta', 
+    'deci', 'ai', 'iz', 'acu', 'hâr', 'pot', 'cel', 'et', 'cea', 'din', 'fan', 'uș', 
+    'but', 'reo', 'pol', 'sec', 'rad', 'bon', 'moto', 'tor', 'exa', 'fon', 'lip']
+
+    sufixes = ['re', 'tor', 'or', 'ic', 'ie', 'ar', 'ist', 'că', 'aș', 
+    'ură', 'al', 'ală', 'ui', 'el', 'iu', 'iță', 'et', 'ier', 'tiv', 
+    'ta', 'dă', 'in', 'ian', 'ci', 'mă', 'tic', 'abil', 'ia', 'ni', 
+    'oare', 'eu', 'na', 'nat', 'st', 'ol', 'tar', 'să', 'rit', 'en', 
+    'ti', 'la', 'atic', 'ca', 'uș', 'ion', 'bă', 'eră', 'și', 
+    'da', 'fon', 'ter', 'ura', 'graf', 'tină', 'nare', 'ce', 'fă', 'va', 
+    'nit', 'pa', 'ei', 'za', 'ut', 'lar', 'urat', 'urare', 'cat', 'di', 'tal', 
+    'care', 'oară', 'mat', 'ai', 'orie', 'ir', 'die', 'dină', 'ga', 'cel', 'ța', 
+    'ici', 'ba', 'cit', 'un', 'tui', 'dare', 'iac', 'scop', 'tura', 'țel', 'lat', 
+    'niu', 'tos', 'logic', 'pune', 'pare', 'sor', 'sat', 'logie', 'de', 'țar', 
+    'fer', 'inic', 'bie', 'var', 'etic', 'pus', 'ler', 'cea', 'tip', 'dat', 
+    'ral', 'aci', 'tea', 'for', 'iar', 'fonie', 'rar', 'ex', 'tist', 'zare', 
+    'pan', 'liță', 'rină', 'ament', 'bi', 'im', 'mer', 'dan', 'sta']
+
+    if w1 in prefixes:
+        return False
+
+    if w2 in sufixes:
+        return False
+
+    return True
+
 def solutions(words, each_min_len, full_len):
     """
     2 esential parameters, for min length of partial word, 
@@ -18,17 +54,13 @@ def solutions(words, each_min_len, full_len):
         for first_word_len in range(each_min_len, each_max_len):
             w1 = w[:first_word_len]
             w2 = w[first_word_len:]
+            if not valid_sol(w1, w2):
+                continue
             if w1 in part_words and w2 in part_words:
                 solution.append((w1, w2))
+    
     solution = solution[::-1]
-
-    #write to file
-    outfile2 = open('solutii_' + str(each_min_len) + '-' + str(full_len) + '.txt', 'w')
-    for s in solution:
-        outfile2.write(s[0] + ' + ' + s[1] + '\n')
-
-    outfile2.close()
-    print("Generated " + 'solutii_' + str(each_min_len) + '-' + str(full_len) + '.txt')
+    
     return solution
 
 def valid(word,words):
@@ -36,43 +68,9 @@ def valid(word,words):
         function used to eliminate words with upper first letter, 
         words that contain spaces and duplicate words
     """
-    prefixes = ['re', 'de', 'co', 'pre', 'in', 'pro', 'di', 'ca', 'cu', 
-    'ba', 'des', 'al', 'bi', 'mi', 'ta', 'tri', 'ex', 'ar', 'st', 'anti', 
-    'para', 'im', 'per', 'ci', 'mono', 'ce', 'că', 'la', 'si', 'dis', 'mă', 
-    'poli', 'or', 'mu', 'fi', 'ga', 'pe', 'cur', 'ha', 'sur', 'col', 
-    'ad', 'no', 'tele', 'bă', 'fa', 'ter', 'aero', 'să', 'pu', 'na', 'va', 
-    'inter', 'câr', 'ti', 'dec', 'for', 'semi', 'sta', 'bur', 'șu', 'uni', 
-    'peri', 'pal', 'ap', 'bor', 'pan', 'ana', 'deca', 'supra', 'sal', 'hi', 
-    'șo', 'și', 'sil', 'tal', 'mus', 'neo', 'op', 'ante', 'cop', 'post', 'ser', 
-    'mol', 'sin', 'bal', 'za', 'ni', 'ho', 'pic', 'mega', 'hă', 'mas', 'înde', 'prea', 
-    'hidro', 'cont', 'tar', 'por', 'pis', 'sul', 'super', 'băl', 'he', 'cam', 'meta', 
-    'deci', 'ai', 'iz', 'acu', 'hâr', 'pot', 'cel', 'et', 'cea', 'din', 'fan', 'uș', 
-    'but', 'reo', 'pol', 'sec', 'rad', 'bon', 'moto', 'tor', 'exa', 'fon', 'lip']
-
-    sufixes = ['re', 'tor', 'or', 'ic', 'ie', 'ar', 'ist', 'că', 'aș', 
-    'ură', 'al', 'ală', 'ui', 'el', 'iu', 'iță', 'et', 'ier', 'tiv', 
-    'ta', 'dă', 'in', 'ian', 'ci', 'mă', 'tic', 'abil', 'ia', 'ni', 
-    'oare', 'eu', 'na', 'nat', 'st', 'ol', 'tar', 'să', 'rit', 'en', 
-    'ti', 'la', 'atic', 'ca', 'uș', 'car', 'ion', 'bă', 'eră', 'și', 
-    'da', 'fon', 'ter', 'ura', 'graf', 'tină', 'nare', 'ce', 'fă', 'va', 
-    'nit', 'pa', 'ei', 'za', 'ut', 'lar', 'urat', 'urare', 'cat', 'di', 'tal', 
-    'care', 'oară', 'mat', 'ai', 'orie', 'ir', 'die', 'dină', 'ga', 'cel', 'ța', 
-    'ici', 'ba', 'cit', 'un', 'tui', 'dare', 'iac', 'scop', 'tura', 'țel', 'lat', 
-    'niu', 'tos', 'logic', 'pune', 'pare', 'sor', 'sat', 'logie', 'de', 'țar', 
-    'fer', 'inic', 'bie', 'var', 'etic', 'pus', 'ler', 'cea', 'tip', 'dat', 
-    'ral', 'aci', 'tea', 'for', 'iar', 'fonie', 'rar', 'ex', 'tist', 'zare', 
-    'pan', 'liță', 'rină', 'ament', 'bi', 'im', 'mer', 'dan', 'sta']
     
     if len(word) == 1:
         return False
-
-    for prefix in prefixes:
-        if word.find(prefix) == 0:
-            return False
-
-    for sufix in sufixes:
-        if word[::-1].find(sufix) == 0:
-            return False
     
     if len(words) > 0:
         if word == words[-1]:
@@ -90,7 +88,6 @@ def create_word_list():
     #create word list
     words = []
     invalid_words = 0
-    i = 1
     infile = open('dex09.txt', 'r')
     for line in infile:
         word = line[:-1]
@@ -98,7 +95,6 @@ def create_word_list():
             invalid_words+=1
             continue    
         words.append(word)  
-        i+=1
     infile.close()
 
     #sort it 
@@ -137,8 +133,28 @@ def reversibles(words):
 
 words = create_word_list()
 
-for j in range(4,10):
-    solution = solutions(words,2,j)
+# mydex = open('mydex.txt', 'r')
+# n = 0
+# words = []
+# for line in mydex:
+#         word = line[:-1]    
+#         words.append(word)  
+#         n += 1
+# mydex.close()
+# print(str(n) + ' words')
+
+solution = []
+for j in range(4,11):
+    solution += solutions(words,2,j)
+    print(str(j) + 'finished')
+
+solution.sort(key = lambda x: x[0])
+
+#write to file
+outfile2 = open('solutii.txt', 'w')
+for s in solution:
+    outfile2.write(s[0] + ' + ' + s[1] + '\n')
+outfile2.close()
 
 
 
